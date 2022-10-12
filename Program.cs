d7c2133bb7db8bffb1ded6b10226d7b7012e81d2
serbianman87 <serbianman87@yahoo.com>
2022-10-12 15:01:41 +0200

    equipment addscoped

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IAdditionalEquipmentRepo, AdditionalEquipmentRepo>();
 builder.Services.AddScoped<IModelRepo, ModelRepo>();
 builder.Services.AddScoped<IMakeRepo, MakeRepo>();
 builder.Services.AddScoped<IVehicleRepo, VehicleRepo>();
