95d148f306d9c893863705e06a2279aad37ae60c
bojanivancev <bojanivancev@gmail.com>
2023-02-20 16:25:08 +0100

    Add TODO comment to Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ using UsedCars.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//TODO
 
 builder.Services.AddControllers();
 builder.Services.AddControllers().AddNewtonsoftJson(setupAction =>
