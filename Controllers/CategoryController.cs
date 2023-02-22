8f31752f962793f27cecf3597f76646254330ecf
serbianman87 <serbianman87@yahoo.com>
2023-02-22 19:52:19 +0100

    Added Category Service and controller

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using UsedCars.Entities;
 using UsedCars.Models;
 using UsedCars.Repository.Category;
+using UsedCars.Services;
 
 namespace UsedCars.Controllers
 {
@@ -11,109 +12,50 @@ namespace UsedCars.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        private readonly ICategoryRepo _categoryRepo;
-
-        private readonly IMapper _mapper;
-
-        public CategoryController(ICategoryRepo categoryRepo, IMapper mapper)
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
         {
-            _categoryRepo = categoryRepo ?? throw new ArgumentNullException(nameof(categoryRepo));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _categoryService = categoryService ?? throw new ArgumentNullException(nameof(categoryService));
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategories()
         {
-            var categories = await _categoryRepo.GetAllAsync();
-
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            return Ok(categories);
+            var categoriesToReturn = _categoryService.GetCategories();
+            return Ok(categoriesToReturn);
         }
         [HttpGet("{categoryId}", Name="GetCategory")]
         public async Task<IActionResult> GetCategory(Guid categoryId)
         {
-            var category = await _categoryRepo.GetById(categoryId);
+            var category = await _categoryService.GetCategory(categoryId);
             if (category==null)
             {
                 return NotFound();
             }
-            return Ok(_mapper.Map<CategoryDto>(category));
+            return Ok(category);
         }
         [HttpPost]
         [ProducesResponseType(204)]
         [ProducesResponseType(400)]
         public ActionResult CreateCategory([FromBody] CategoryDto categoryCreate)
         {
-           
-
-            var categoryEntity = _mapper.Map<Category>(categoryCreate);
-            _categoryRepo.Insert(categoryEntity);
-            _categoryRepo.Save();
-
-            var categoryToReturn = _mapper.Map<CategoryDto>(categoryEntity);
-
+            var categoryToReturn = _categoryService.CreateCategory(categoryCreate);
             return Ok(categoryToReturn);
-        }
-
-        [HttpPut("{categoryId}")]
-        public async Task<IActionResult> UpdateCategory(Guid categoryId, [FromBody] UpdateCategoryDto category)
-        {
-            if (!_categoryRepo.CategoryExsits(categoryId))
-            {
-                return NotFound();
-            }
-
-            var categoryFromRepo = _categoryRepo.GetById(categoryId);
-
-            if (categoryFromRepo == null)
-            {
-                var categoryToAdd = _mapper.Map<Category>(category);
-                categoryToAdd.Id = categoryId;
-
-                _categoryRepo.Insert(categoryToAdd);
-
-                _categoryRepo.Save();
-
-                var categoryToReturn = (_mapper.Map<CategoryDto>(categoryToAdd));
-
-                return CreatedAtRoute("GetCategory", new { categoryId = categoryToReturn.Id }, categoryToReturn);
-               
-            }
-
-            _mapper.Map(category, categoryFromRepo);
-
-            _categoryRepo.Update(await categoryFromRepo);
-
-            _categoryRepo.Save();
-
-            return NoContent();
-            
         }
 
         [HttpDelete("{categoryId}")]
         public ActionResult DeleteCategory(Guid categoryId)
         {
-            var categoryFromRepo = _categoryRepo.GetById(categoryId);
-
+            var categoryFromRepo = _categoryService.DeleteCategory(categoryId);
+            
             if (categoryFromRepo==null)
             {
                 return NotFound();
             }
-
-            _categoryRepo.Delete(categoryFromRepo);
-            _categoryRepo.Save();
-
+                      
             return NoContent();
-
+            
         }
-
-
-
-
-     
-
-
+        
     }
 }
